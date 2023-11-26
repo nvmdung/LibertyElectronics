655b07ff6945f16fc38a6301a1bf2d36deaff2d6
tapcanbinh222 <lethanhtrung28042000@gmail.com>
2023-11-26 20:52:54 +0700

    sửa lại css

--- source/src/components/Item2.js
+++ source/src/components/Item2.js
@@ -1,13 +1,15 @@
 import { Navigate, useNavigate } from "react-router-dom";
+import style from '../css/Home.css';
+
 function Item2({laptop}){
     const navigate = useNavigate();
     return(
         <div>
         <tr key={laptop.id}>
         <div>
-        <img src={laptop.image[0]} alt="laptop"style={{ width: '150px', height: 'auto' }}/>
-        
-        <td>{laptop.price}</td>
+        <img src={laptop.image[0]} alt="laptop"style={{ width: '120px', height: 'auto' }}/>
+        <td>{laptop.name}</td>
+        <p>{laptop.price}</p>
         </div>
             <td>
                 {/* <button onClick={() => navigate(`/details/${laptop.id}`)}>Details</button> */}
