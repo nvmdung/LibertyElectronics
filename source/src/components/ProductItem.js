4f11ad726097326ab2cf7d6b55221873082734b6
thanhlong2001 <phucvatu456@gmail.com>
2023-12-01 16:27:52 +0700

    fix

--- source/src/components/ProductItem.js
+++ source/src/components/ProductItem.js
@@ -2,20 +2,29 @@ import classes from "../css/Product.module.css"
 import { useNavigate } from "react-router-dom";
 function ProductItem({laptop, addCart, getDetails}){
     const navigate = useNavigate();
+    const shortenProductName = (name, maxLength) => {
+        if (name.length <= maxLength) {
+          return name;
+        } else {
+          return name.substring(0, maxLength) + '...';
+        }
+      };
+
     return(
     <tr>
         <td onClick={()=> {
             getDetails(laptop);
             navigate('/details');
         }}>
-        <img src={laptop.image[0]} className={classes.prodimg}/>
+          <div className={classes.prodimg}><img src={laptop.image[0]} width="100px"/></div>
         </td>
-        <div><td>{laptop.name}</td></div>
-        <div><td>${laptop.price}</td></div>
+        <div className={classes.prodimg}><td> {shortenProductName(laptop.name, 10)}</td></div>
+        <div className={classes.prodimg}><td>${laptop.price}</td></div>
+        <div>
         <td>
             <button onClick={() => addCart(laptop)}>Add to Cart</button>
-            
         </td>
+        </div>
     </tr>
     )
 }
--- source/src/components/ProductList.js
+++ source/src/components/ProductList.js
@@ -1,29 +1,37 @@
-
-
-import classes from '../css/Product.module.css'
+import React, { useState } from 'react';
+import classes from '../css/Product.module.css';
 import ProductItem from './ProductItem';
 
-function ProductList({laptops,addCart,getDetails}){
+function ProductList({ laptops, addCart, getDetails }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4; // Điều chỉnh số sản phẩm hiển thị trên mỗi trang nếu cần
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentLaptops = laptops.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(laptops.length / itemsPerPage);
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
-    return(
-        <div>
-               <div className={classes.header}>
-                <h1>Laptop List</h1>
-            </div>
-            <thead>
-            </thead>
-            <tbody>
-                <div className={classes.boxes}>
-            {
-                    laptops.map(p => (
-                        <ProductItem key={p.id} laptop={p}  addCart={addCart} getDetails={getDetails}/>
-                    ))
-                }
-                </div>
-            </tbody>
-
-
-        </div>
-    );
+  return (
+    <div>
+      <div className={classes.header}>
+        <h1>Danh sách Laptop</h1>
+      </div>
+      <div className={classes.boxes}>
+        {currentLaptops.map((p) => (
+          <ProductItem key={p.id} laptop={p} addCart={addCart} getDetails={getDetails} />
+        ))}
+      </div>
+      <div className={classes.pagination}>
+        {Array.from({ length: totalPages }).map((_, index) => (
+          <button key={index} onClick={() => handlePageChange(index + 1)}>
+            {index + 1}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
+
 export default ProductList;
