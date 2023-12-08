b7f5bc16c1253e63bc327bfa77a532241586960c
tapcanbinh222 <lethanhtrung28042000@gmail.com>
2023-12-08 16:18:31 +0700

    Update Home.js

--- source/src/components/Home.js
+++ source/src/components/Home.js
@@ -4,8 +4,8 @@ import Images from "./Fourboxhome";
 import { Link } from "react-router-dom";
 import BrandLogos from "./BrandLogo";
 import Twobox from "./Twoboxhome";
-import Item from "./Item";
-function Home(avatarLaptopsAsus){
+import Itemasus from "./Item";
+function Home(avatarLaptopsAsus ,addCart){
     return(
         <>
      <CarouselHome/>
@@ -13,10 +13,7 @@ function Home(avatarLaptopsAsus){
       <Link to="/product"><div className="showproduct">Show all Products</div></Link>
       <BrandLogos/>
       <Twobox/>
-      <div>
-        {avatarLaptopsAsus.slice(1,2).map(a => (
-            <div><Item key={a.id}  laptop={a}/></div>
-        ))}</div>
+      
     </>
     )
 }export default Home;
