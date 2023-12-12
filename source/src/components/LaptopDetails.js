696707a1ec52c8a2ba60159dbc743c546c98b9a8
tapcanbinh222 <lethanhtrung28042000@gmail.com>
2023-12-12 16:38:22 +0700

    a

--- source/src/components/LaptopDetails.js
+++ source/src/components/LaptopDetails.js
@@ -1,28 +1,52 @@
+import { useState } from 'react';
 import '../css/LaptopDetails.css';
+
 function LaptopDetails({laptop,addCart}){
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+  };
     return(
         <div className="laptopDetails">
-            { <div>
-                {laptop.image.map( i => (
-            <img src={i}width="300px"/>))}
-            </div> }
             <div>
-            <div class="row">
-          <div class="col-xs-8">
+            <div className="image-gallery-container">
+            <div className="large-image">
+        {selectedImage ? (
+          <img src={selectedImage} alt="Selected" className="selected-image" />
+        ) : (
+          <p>{laptop.image.slice(0,1).map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt="laptop" className='large-detailimg'
+              onClick={() => handleImageClick(image)}
+            />  ))}</p>
+        )}
+      </div>
+      <div className="small-images">
+        {laptop.image.slice(0,5).map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Image ${index + 1}`}
+            onClick={() => handleImageClick(image)}
+            className="small-image"
+          />
+        ))}
+      </div>
+     
+    </div>
+          <div>
             <h3>Specifications</h3>
-            <div class="panel-info">
-              <h3>Basic parameters</h3>
+            
               <div class="row">
                 <div class="col-xs-4">Screensize:
-               <span>{laptop.screensize}</span></div>
+               <span class="col-xs-8">{laptop.screensize}</span></div>
               </div>
               <div class="row">
                 <div class="col-xs-4">Weight:
                 <span class="col-xs-8">{laptop.itemweight}</span></div>
-              </div>
-              <div class="row">
-                <div class="col-xs-4">Camera sau:	
-                <span class="col-xs-8">20 MP</span></div>
               </div>
               <div class="row">
                 <div class="col-xs-4">CPU:
@@ -45,12 +69,12 @@ function LaptopDetails({laptop,addCart}){
                 <span class="col-xs-8">{laptop.warranty}</span></div>
               </div>
             </div>
-        </div>
+        
         </div>
                 <button onClick={()=> addCart(laptop)}>Add to cart</button>
                 <a href="www.google.com" download="laptop_detail">Download</a>       
             </div>
-        </div>
+        
     )
 }
 export default LaptopDetails;
