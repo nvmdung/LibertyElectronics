87c6333dc88220050203b2be7ab208cbb70eb32a
tapcanbinh222 <lethanhtrung28042000@gmail.com>
2023-12-12 23:11:41 +0700

    detail

--- source/src/components/LaptopDetails.js
+++ source/src/components/LaptopDetails.js
@@ -1,16 +1,26 @@
 import { useState } from 'react';
 import '../css/LaptopDetails.css';
 
+
 function LaptopDetails({laptop,addCart}){
   const [selectedImage, setSelectedImage] = useState(null);
 
   const handleImageClick = (image) => {
     setSelectedImage(image);
   };
+  const handleAddToCart = (laptop) => {
+    addCart(laptop);
+};
+const calculateDiscountedPrice = (price, discount) => {
+  return (price * (100 - discount)) / 100;
+};
+
+const discountedPrice = calculateDiscountedPrice(laptop.price, laptop.discount || 0);
     return(
         <div className="laptopDetails">
             <div>
             <div className="image-gallery-container">
+            <div className='gridimagebrand'>
             <div className="large-image">
         {selectedImage ? (
           <img src={selectedImage} alt="Selected" className="selected-image" />
@@ -22,10 +32,54 @@ function LaptopDetails({laptop,addCart}){
               alt="laptop" className='large-detailimg'
               onClick={() => handleImageClick(image)}
             />  ))}</p>
-        )}
+        )}</div>
+        <div className='detailpro'>
+          <p className='detailnamelaptop'>{laptop.name}</p>
+          <p >{laptop.discount ? (
+              <div className="laptopdetailprice">
+                <span className="originallaptopdetailprice">${laptop.price}</span><br/>
+                ${discountedPrice.toFixed(2)} (-{laptop.discount}%)
+              </div>
+            ) : (
+              <div className="laptopdetailprice1">${laptop.price}</div>
+            )}</p>
+
+          <div onClick={() => handleAddToCart(laptop)} className="addcardetail">
+            <div class="buttondetaillaptop" data-tooltip={`$${laptop.price}`}>
+  <div class="buttondetaillaptop-wrapper">
+    <div class="textdetaillaptop">Add To Cart</div>
+    <span class="icondetaillaptop">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-cart2"
+        viewBox="0 0 16 16">
+        <path
+          d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"
+        ></path>
+      </svg>
+    </span>
+  </div>
+
+</div>
+<div class = "purchase-info">        
+        <button class="btn">Compare</ button>
+      </div>
+</div>
+<div class = "product-detail-order">
+<ul>
+          <li>Available: <span>in stock</span></li>
+          <li>Category: <span>Laptop</span></li>
+          <li>Shipping Area: <span>All over the world</span></li>
+          <li>Shipping Fee: <span>Free</span></li>
+        </ul>
+        </div>
+        </div>
       </div>
       <div className="small-images">
-        {laptop.image.slice(0,5).map((image, index) => (
+        {laptop.image.slice(0,4).map((image, index) => (
           <img
             key={index}
             src={image}
@@ -35,10 +89,9 @@ function LaptopDetails({laptop,addCart}){
           />
         ))}
       </div>
-     
     </div>
           <div>
-            <h3>Specifications</h3>
+            <h3 className='h3laptopdetail'>Specifications</h3>
             
               <div class="row">
                 <div class="col-xs-4">Screensize:
@@ -50,7 +103,7 @@ function LaptopDetails({laptop,addCart}){
               </div>
               <div class="row">
                 <div class="col-xs-4">CPU:
-                <span class="col-xs-8">{laptop.cpuchip}t</span></div>
+                <span class="col-xs-8">{laptop.cpuchip}</span></div>
               </div>
               <div class="row">
                 <div class="col-xs-4">RAM:
@@ -71,7 +124,7 @@ function LaptopDetails({laptop,addCart}){
             </div>
         
         </div>
-                <button onClick={()=> addCart(laptop)}>Add to cart</button>
+       
                 <a href="www.google.com" download="laptop_detail">Download</a>       
             </div>
         
