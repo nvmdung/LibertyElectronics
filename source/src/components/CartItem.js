773dad13af3d349abc92e74a6179566f1cd803e8
tapcanbinh222 <lethanhtrung28042000@gmail.com>
2023-12-18 20:24:08 +0700

    a

--- source/src/components/CartItem.js
+++ source/src/components/CartItem.js
@@ -1,20 +1,45 @@
 import { formatCurrency } from "../components/Currency";
-function CartItem({product, deleteCart, decreaseQty, increaseQty}){
-    return(
-        <tr>
-            <td>{product.name}</td>
-            <td><img src={product.image[0]} alt="image" width="150px"/></td>
-            <td>$ {product.price}</td>
-            <td>
-                <button onClick={() => decreaseQty(product)}>-</button>
-                {product.quantity}
-                <button onClick={() => increaseQty(product)}>+</button>
+import Table from 'react-bootstrap/Table';
+import '../css/Cart.css'
 
-               
-            </td>
-            <td>{formatCurrency(product.quantity * product.price)}</td>
-            <td><button onClick={() => deleteCart(product)}>Delete</button></td>
+function CartItem({ product, deleteCart, decreaseQty, increaseQty }) {
+    const totalPrice = product.price * product.quantity;
+
+    return (
+        <Table striped bordered hover>
+            <thead className="cartlist">
+                <tr>
+                    <th>id</th>
+                    <th>Name</th>
+                    <th>Product</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Total</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>{product.id}</th>
+                    <th>{product.name}</th>
+                    <th><img src={product.image[0]} alt="image" width="150px" /></th>
+                    <th>$ {product.price}</th>
+                    <th>
+                        <button onClick={() => decreaseQty(product)}>-</button>
+                        {product.quantity}
+                        <button onClick={() => increaseQty(product)}>+</button>
+                    </th>
+                    <th>{formatCurrency(product.quantity * product.price)}</th>
+                    <th><button onClick={() => deleteCart(product)}>Delete</button></th>
+                </tr>
+                <tr>
+              <td></td>
+              <td colSpan={2}>Total</td>
+              <td>{formatCurrency(totalPrice)}</td>
         </tr>
-    )
+            </tbody>
+        </Table>
+    );
 }
+
 export default CartItem;
--- source/src/components/CartList.js
+++ source/src/components/CartList.js
@@ -1,43 +1,26 @@
 import CartItem from "./CartItem";
-import { formatCurrency } from "../components/Currency";
+
 
 function CartList({carts,deleteCart, decreaseQty, increaseQty}){
     
-    const totalPrice = carts.reduce((total, product) => {
-        return total + (product.price * product.quantity);
-    }, 0);
+   
     if (!carts || carts.length === 0) {
         return (
-            <div >
+            <div className="cartlist">
                 <h3>No products in cart</h3>
             </div>
         );
     }
     return(
-        <div>
+        <div  className="cartlist">
             <h2>Cart List</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Image</th>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
-                        <th>Total</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
+            <table> 
                 <tbody>
                         {carts.map(product => (
                             <CartItem key={product.id} product={product} deleteCart={deleteCart} increaseQty={increaseQty} decreaseQty={decreaseQty} />
                         ))}
                 </tbody>
             </table>
-                <div >
-                    
-                    <div > <h5>Total : {formatCurrency(totalPrice)}</h5></div>
-                </div>
-                
         </div>
     )
 }
