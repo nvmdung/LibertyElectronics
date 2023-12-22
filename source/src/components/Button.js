50a7a2a1c9c865b0a1d14913dff64a33770f9d01
thanhlong2001 <phucvatu456@gmail.com>
2023-12-22 16:29:56 +0700

    fix

--- source/src/components/Button.js
+++ source/src/components/Button.js
@@ -1,30 +1,33 @@
-import '../css/Button.css'
+import '../css/Button.css';
 import { useNavigate } from 'react-router-dom';
-function Button(){
-    const navigate=useNavigate();
-    return(
-        <div className='container-btn5'>
-        <tr className='container-productbutton'>
-          <td className='btn-91'><button onClick={() => {
-            navigate('/asus');
-          }}><span>ASUS</span></button></td>
-          <td className='btn-91'><button onClick={()=>{
-            navigate('/lenovo');
-          }}><span>LENOVO</span></button></td>
-          <td className='btn-91'><button onClick={() => {
-           navigate('/msi');
-          }}><span>MSI</span></button></td>
-          <td className='btn-91'><button onClick={() => {
-           navigate('/gigabyte');
-          }}><span>GIGABYE</span></button></td>
-          <td className='btn-91'><button onClick={() =>{ 
-           navigate('/hp');
-         }}><span>HP</span></button></td>
-          <td className='btn-91'><button onClick={() => {
-           navigate('/dell');
-          }}><span>DELL</span></button></td>
-        </tr>
+
+function Button() {
+  const navigate = useNavigate();
+
+  return (
+    <div className='container-btn5'>
+      <div className='container-productbutton'>
+        <button className='btn-91' onClick={() => navigate('/asus')}>
+          <span>ASUS</span>
+        </button>
+        <button className='btn-91' onClick={() => navigate('/lenovo')}>
+          <span>LENOVO</span>
+        </button>
+        <button className='btn-91' onClick={() => navigate('/msi')}>
+          <span>MSI</span>
+        </button>
+        <button className='btn-91' onClick={() => navigate('/gigabyte')}>
+          <span>GIGABYE</span>
+        </button>
+        <button className='btn-91' onClick={() => navigate('/hp')}>
+          <span>HP</span>
+        </button>
+        <button className='btn-91' onClick={() => navigate('/dell')}>
+          <span>DELL</span>
+        </button>
       </div>
-    );
+    </div>
+  );
 }
+
 export default Button;
