0926b3a4324ef5cbd968216a87408d20d6c4e23f
tapcanbinh222 <lethanhtrung28042000@gmail.com>
2023-12-22 14:31:52 +0700

    a

--- source/src/components/CartItem.js
+++ source/src/components/CartItem.js
@@ -1,25 +1,23 @@
 import { formatCurrency } from "../components/Currency";
-import Table from 'react-bootstrap/Table';
+
 import '../css/Cart.css'
 
 function CartItem({ product, deleteCart, decreaseQty, increaseQty }) {
 
     return (
-     <div>
-       
-           
+     <div>    
             <tbody>
                 <tr>
                     <td className="cartlistid"><div>{product.id}</div></td>
                     <th className="cartlistproduct"><img src={product.image[0]} alt="image" width="140px" /></th>
                     <td className="cartlistname"><div>{product.name}</div></td>
-                    <th className="cartlistprice"><div>${product.price}</div></th>
+                    <th className="cartlistprice"><div>${product.discount ? (product.price - (product.price * (product.discount / 100))) : product.price}</div></th>
                     <th className="cartlistquantity">
                         <div className="buttonminus" onClick={() => decreaseQty(product)}>-</div>
                         {product.quantity}
                         <div className="buttonminus" onClick={() => increaseQty(product)}>+</div>
                     </th>
-                    <th className="cartlisttotal">{formatCurrency(product.quantity * product.price)}</th>
+                    <th className="cartlisttotal">{formatCurrency((product.quantity * (product.discount ? (product.price - (product.price * (product.discount / 100))) : product.price)))}</th>
                     <th className="cartlistaction">
                     <div class=" delete-button-box" onClick={() => deleteCart(product)}>                 
                             <button class=" delete-button" >Delete</button>
@@ -30,6 +28,6 @@ function CartItem({ product, deleteCart, decreaseQty, increaseQty }) {
            
             </div>
     );
-}
+}   
 
 export default CartItem;
