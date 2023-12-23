9285a8c0f6225252d7b1f6da10526a8236be6e72
tapcanbinh222 <lethanhtrung28042000@gmail.com>
2023-12-23 18:27:11 +0700

    fix lỗi giá

--- source/src/components/CartItem.js
+++ source/src/components/CartItem.js
@@ -11,13 +11,20 @@ function CartItem({ product, deleteCart, decreaseQty, increaseQty }) {
                     <td className="cartlistid"><div>{product.id}</div></td>
                     <th className="cartlistproduct"><img src={product.image[0]} alt="image" width="140px" /></th>
                     <td className="cartlistname"><div>{product.name}</div></td>
-                    <th className="cartlistprice"><div>${product.discount ? (product.price - (product.price * (product.discount / 100))) : product.price}</div></th>
+                    <th className="cartlistprice">
+                        <div>
+                        ${product.discount ? 
+                            ((product.price - (product.price * (product.discount / 100))).toFixed(2)) : (product.price.toFixed(2))}
+                        </div>
+                    </th>
+
                     <th className="cartlistquantity">
                         <div className="buttonminus" onClick={() => decreaseQty(product)}>-</div>
                         {product.quantity}
                         <div className="buttonminus" onClick={() => increaseQty(product)}>+</div>
                     </th>
-                    <th className="cartlisttotal">{formatCurrency((product.quantity * (product.discount ? (product.price - (product.price * (product.discount / 100))) : product.price)))}</th>
+                    <th className="cartlisttotal">{formatCurrency((product.quantity * (product.discount ? (product.price - (product.price * (product.discount / 100))) : product.price)).toFixed(2))}</th>
+
                     <th className="cartlistaction">
                     <div class=" delete-button-box" onClick={() => deleteCart(product)}>                 
                             <button class=" delete-button" >Delete</button>
